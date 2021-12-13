768aaed4e55d179f3433c42d2851a222cf4c71ed
parth dhaduk <dhadukparth9@gmail.com>
2021-12-13 17:21:21 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title: "My App",
+      home:  Scaffold(.
+        appBar:  AppBar(
+          title: Text('My First App'),
+        ),
+        body:  Text('This is my default text')
+         ,
+      ),
+    );
+  }
+}
+
